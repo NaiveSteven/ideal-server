f24eb96be0bdfee6d74bf0c7f68d84ee2a176bae
qinmingjie <1004537433@qq.com>
2020-10-07 03:01:46 +0800

    feat(inite)

---
+++ src/app.ts
@@ -0,0 +1,55 @@
+import configs from './configs';
+import Koa, { Context } from 'Koa';
+import { bootstrapControllers } from 'koa-ts-controllers';
+import KoaRouter from 'koa-router';
+import path from 'path';
+import KoaBodyParser from 'koa-bodyparser';
+import Boom from '@hapi/Boom';
+
+(async () => {
+  const app = new Koa();
+
+  const router = new KoaRouter();
+
+  // 注册路由
+  await bootstrapControllers(app, {
+    router,
+    basePath: '/api',
+    versions: [1],
+    controllers: [
+      path.resolve(__dirname, 'controllers/**/*')
+    ],
+    errorHandler: async (err: any, ctx: Context) => {
+      let status = 500;
+      let body: any = {
+        status: status,
+        error: 'error',
+        message: 'error message'
+      };
+
+      if (err.output) {
+        status = err.output.statusCode;
+        body = {
+          ...err.output.payload
+        };
+        if (err.data) {
+          body.errorDetails = err.data;
+        }
+      }
+      ctx.status = status;
+      ctx.body = body;
+    }
+  });
+
+  // router.all('*', async ctx => {
+  //   throw Boom.notFound('没有该路由');
+  // });
+
+  app.use(KoaBodyParser());
+  app.use(router.routes());
+
+  app.listen(configs.server.port, configs.server.host, () => {
+    console.log(`服务启动成功: http: //${configs.server.host}:${configs.server.port}`);
+  })
+})();
+
---
+++ src/controllers/Test.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from "koa-ts-controllers";
+
+@Controller('/test')
+class TestController {
+  
+  @Get('/hello')
+  async hello() {
+    return 'hello';
+  }
+}
