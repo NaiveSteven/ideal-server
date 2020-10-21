f3e560b135cb399efa8d2c13479c108bd904c40d
qinmingjie <1004537433@qq.com>
2020-10-21 17:13:52 +0800

    feat(Promotion和Explosion数据表)

---
+++ src/database/seeders/initPromotion.js
@@ -0,0 +1,33 @@
+
+module.exports = {
+  up(queryInterface, Sequelize) {
+    let date = new Date();
+    let goodsTypeId = [1, 2, 2, 3, 4, 2, 5, 5, 7, 10, 1, 2, 3, 4, 5, 6, 7];
+    const brandId = [1, 2, 2, 3, 4, 2, 5, 5, 7, 10, 1, 2, 3, 4, 5, 6, 7];
+    const imageUrl =
+    ['asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf'];
+    const state = [1, 2, 2, 3, 1, 2, 3, 3, 2, 1, 2, 3, 2, 1, 1, 1, 1];
+
+    return queryInterface.bulkInsert('Promotion', goodsTypeId.map((typeId, index) => {
+      let id = index + 1;
+      return {
+        id,
+        goodsTypeId: typeId,
+        brandId: brandId[index],
+        name: 'Goods-' + id,
+        desc: 'desc-' + id,
+        price: 10 + index,
+        count: 10 + index,
+        size: '5' + index + 'g',
+        marketPrice: 15 + index,
+        imageUrl: imageUrl[index],
+        state: state[index],
+        createdAt: date,
+        updatedAt: date
+      }
+    }));
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Promotion', null, {});
+  }
+};
---
+++ src/models/Explosion.ts
@@ -0,0 +1,101 @@
+import {
+  AutoIncrement,
+  Column,
+  CreatedAt,
+  DataType,
+  ForeignKey, HasMany,
+  Model,
+  PrimaryKey,
+  Table,
+  UpdatedAt
+} from "sequelize-typescript";
+import { GoodsType } from './GoodsType';
+import { Brand } from './Brand';
+
+@Table({
+  tableName: 'Explosion'
+})
+export class Explosion extends Model<Explosion> {
+
+  @PrimaryKey
+  @AutoIncrement
+  @Column
+  id: number;
+
+  @ForeignKey(() => GoodsType)
+  @Column({
+    type: DataType.INTEGER.UNSIGNED,
+    allowNull: false
+  })
+  goodsTypeId: number;
+
+  @ForeignKey(() => Brand)
+  @Column({
+    type: DataType.INTEGER.UNSIGNED,
+    allowNull: false
+  })
+  brandId: number;
+
+  // 名称
+  @Column({
+    type: DataType.STRING(255),
+    allowNull: false
+  })
+  name: string;
+
+  // 描述
+  @Column({
+    type: DataType.STRING(255),
+    allowNull: false
+  })
+  desc: string;
+
+  // 价格
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false
+  })
+  price: number;
+
+  // 市场价
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false
+  })
+  marketPrice: number;
+
+  // 图片
+  @Column({
+    type: DataType.STRING(255),
+    allowNull: false
+  })
+  imageUrl: string;
+
+  // 规格
+  @Column({
+    type: DataType.STRING(255),
+    allowNull: false
+  })
+  size: string;
+
+  // 状态 销售中 下架 售罄
+  @Column({
+    type: DataType.STRING(255),
+    allowNull: false
+  })
+  state: number;
+
+  // 数量（库存）
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false
+  })
+  count: number;
+
+  @CreatedAt
+  createdAt: Date;
+
+  @UpdatedAt
+  updatedAt: Date;
+
+}
