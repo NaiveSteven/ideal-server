8f2c4f7a9dcaa81f27f4b11bc6a2c3cb6b0fe5fe
qinmingjie <1004537433@qq.com>
2020-11-08 23:35:18 +0800

    refactor(user代码优化)

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -16,20 +16,22 @@ class UserController {
     @Ctx() ctx: Context,
     @Body() body: UserListBody
   ) {
-    const limit = Number(body.limit);
-    const offset = (Number(body.page) - 1) * limit;
-    const { adminUserId } = body;
-    const searchObj = getUncertainSqlObj({ adminUserId });
-    const userList = await UserModel.findAndCountAll({
-      where: {
-        ...searchObj
-      },
-      limit,
-      offset,
-      attributes: ['id', 'name', 'createdAt', 'updatedAt']
-    });
-    return {
-      userList
+    try {
+      const limit = Number(body.limit);
+      const offset = (Number(body.page) - 1) * limit;
+      const { adminUserId } = body;
+      const searchObj = getUncertainSqlObj({ adminUserId });
+      const userList = await UserModel.findAndCountAll({
+        where: {
+          ...searchObj
+        },
+        limit,
+        offset,
+        attributes: ['id', 'name', 'createdAt', 'updatedAt']
+      });
+      return resMsg(200, userList, 1);
+    } catch (error) {
+      return resMsg();
     }
   }
 }
